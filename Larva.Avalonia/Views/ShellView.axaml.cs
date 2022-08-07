cbe0f8af8d14b2b21f3b76da3b6a1ad8a49ac75c
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-08-07 05:42:00 +0300

    Fix a "bug" with the project's menu item hit area.

--- Larva.Avalonia/Views/ShellView.axaml.cs
+++ Larva.Avalonia/Views/ShellView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
 using Larva.Avalonia.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,5 +12,20 @@ public sealed partial class ShellView : Window
     {
         DataContext = App.Current?.Services.GetRequiredService<ShellViewModel>();
         InitializeComponent();
+    }
+
+    private void RectangleOnPointerEnter(object? sender, PointerEventArgs eventArgs)
+    {
+        ProjectMenuItem.Background = (IBrush?) App.Current?.FindResource("MenuFlyoutItemBackgroundPointerOver");
+    }
+
+    private void RectangleOnPointerLeave(object? sender, PointerEventArgs eventArgs)
+    {
+        ProjectMenuItem.Background = (IBrush?) App.Current?.FindResource("MenuFlyoutItemBackground");
+    }
+
+    private void RectangleOnPointerPressed(object? sender, PointerPressedEventArgs eventArgs)
+    {
+        ProjectMenuItem.IsSubMenuOpen = true;
     }
 }
